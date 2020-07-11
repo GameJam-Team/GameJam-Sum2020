7a32ca4b60853b034ff91158862ec5b179ad7ffc
Yan Gasparyunas <dex_yandex@icloud.com>
2020-07-11 23:46:26 +0400

    Added Water, and interactions with water. Some balance changes, bug fixes.

--- GameForJam/Assets/Scripts/HealthController.cs
+++ GameForJam/Assets/Scripts/HealthController.cs
@@ -6,6 +6,8 @@ public class HealthController : MonoBehaviour
 {
    public bool immortal;
     [SerializeField] private uint _health;
+        public float _oxigen, _energy;
+
     private ParticleSystem _particleSystem;
     private void Awake()
     {
@@ -28,5 +30,27 @@ public class HealthController : MonoBehaviour
             }
         }
         
+    }
+    public void increaseOxigen()
+    {if (_oxigen < 100) 
+        _oxigen += 0.5f;
+    }
+    public void discreaseOxygen()
+    {
+        _oxigen -= 0.4f;
+        if (_oxigen <= 0) Destroy(gameObject);
+    }
+    public void increaseEnergy()
+    {if (_energy < 100) 
+        _energy += 0.01f;
+    }
+    public bool discreaseEnergy ( float decrement)
+    {
+        if (_energy > decrement)
+        {
+            _energy -= decrement;
+            return true;
+        }
+        else return false;
     }
 }
---
+++ GameForJam/Assets/Scripts/breath.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class breath : MonoBehaviour
+{
+    private bool air = true;
+   private HealthController HP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP = GetComponentInParent<HealthController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (air) HP.increaseOxigen();
+        else HP.discreaseOxygen();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+        if (collision.CompareTag("water"))
+        {
+            
+            air = false;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("water"))
+
+            air = true;
+    }
+}
