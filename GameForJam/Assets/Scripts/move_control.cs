b9e9accba6d93e1ccb2e726c6d06e6ce4ac97f01
Yan Gasparyunas <dex_yandex@icloud.com>
2020-07-11 16:24:02 +0400

    Bug fixes, optimization. Added new phisical material whithot friction.

--- GameForJam/Assets/Scripts/move_control.cs
+++ GameForJam/Assets/Scripts/move_control.cs
@@ -7,90 +7,119 @@ public class move_control : MonoBehaviour
     public  float shft_distanse = 13;
     public  float cd_shft_time = 2;
     //public int y;
-    float y, x,tall,enviroment_speed_coef,shft_cd;
+    float x,tall,enviroment_speed_coef,shft_cd;
     public float speed, jmp_speed;
-    Vector3 direction, shift;
+    Vector3 shift;
     // Start is called before the first frame update
     CapsuleCollider2D height;
-     bool jump, ground,shift_act;
+     bool down,jump, ground,shift_act;
+    Rigidbody2D phis;
+    Stack <float> coefs = new Stack<float>();
     void Start()
     {
+        coefs.Push(1);
         enviroment_speed_coef = 1;
         height = gameObject.GetComponent<CapsuleCollider2D>();
         tall = height.size.y;
         jump = false;
         ground = true;
         shift_act = false;
+        down = false;
+        phis = this.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
         /*if (jump && ground && Input.GetAxis("Vertical") <= 0)
             jump = false;*/
-        if (!Input.GetKey(KeyCode.W)&& !Input.GetKey("space") && ground)
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey("space") && ground)
             // if (jump && ground /*&& Input.GetAxis("Vertical") <= 0*/)
             jump = false;
-        if ((Input.GetKeyDown(KeyCode.W)|| Input.GetKeyDown("space")) && !jump)
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown("space")) && !jump&& ground)
         {
             jump = true;
-            ground = false;
-            this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jmp_speed, ForceMode2D.Impulse);
-            
-        }
+           // ground = false;
+            phis.AddForce(Vector2.up * jmp_speed, ForceMode2D.Impulse);
 
-
-        if (Input.GetKeyDown(KeyCode.S) && !jump && ground)
-        {
-            height.size = new Vector2(height.size.x, tall / 1.75f);
-            enviroment_speed_coef = 0.19f;
-        }
-
-
-        if (Input.GetKeyUp(KeyCode.S) || !ground)
-        {
-            height.size = new Vector2(height.size.x, tall);
-            if (Input.GetKeyUp(KeyCode.S))
-                enviroment_speed_coef = 1;
         }
         
+
+        if ( ground&& !jump &&Input.GetKey(KeyCode.S) && !down)
+        {
+            height.size = new Vector2(height.size.x, tall / 2f);
+            coefs.Push(enviroment_speed_coef);
+            enviroment_speed_coef = 0.35f;
+            down = true;
+
+        }
+
+
+        if ((Input.GetKeyUp(KeyCode.S) || !ground)&&down)
+        {
+            height.size = new Vector2(height.size.x, tall);
+           // if (Input.GetKeyUp(KeyCode.S)&& ground)
+                enviroment_speed_coef = coefs.Pop();
+            down = false;
+        }
+
     }
            
     
 
     private void FixedUpdate()
     {
-       
-        x = Input.GetAxis("Horizontal");
         
+
+        x = Input.GetAxis("Horizontal");
+        if (x > 0) x = 1f;
+        if (x < 0) x = -1f;
         if (Input.GetKey(KeyCode.LeftShift) && !shift_act&& Mathf.Abs( x)>0&& shft_cd<=0)
         {
             shift = this.transform.position;
             shift_act = true;
-            enviroment_speed_coef = 10;
-            y= x = 1* Mathf.Sign(x);
+            coefs.Push(enviroment_speed_coef);
+            enviroment_speed_coef = 15;
+            //y= x = 1* Mathf.Sign(x);
             shft_cd = cd_shft_time;
+            //this.GetComponent<Rigidbody2D>().AddForce(Vector2.right * speed * x * enviroment_speed_coef, ForceMode2D.Impulse);
         }
         if (shift_act)
         {
-            x = y;
+           // x = y;
+            
             if (Vector3.Distance(shift, this.transform.position) > shft_distanse)
             {
                 shift_act = false;
-                enviroment_speed_coef = 1;
+                enviroment_speed_coef = coefs.Pop();    
             }
         }
         if (shft_cd > 0)
             shft_cd -= Time.deltaTime;
-            
-        direction = new Vector3(x, 0, 0);
-        this.transform.position = this.transform.position + direction * speed* enviroment_speed_coef;             
+
+        //direction = new Vector3(x, 0, 0);
+       // if (!shift_act)
+        phis.velocity = new Vector2(x * speed * enviroment_speed_coef, phis.velocity.y);           
     }
                
     void OnCollisionEnter2D(Collision2D collision)
     {
+       
+        if ((collision.gameObject.tag == "Wall"|| collision.gameObject.tag == "earth") && shift_act)
+        {
+            shift_act = false;
+            enviroment_speed_coef = 1;
+        }
+    }
+   void OnTriggerEnter2D(Collider2D collision)
+    {
         if (collision.gameObject.tag == "earth")
-           ground = true;
-
+         ground = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "earth")
+            ground = false;
+        Debug.Log("Fff");
     }
 }
